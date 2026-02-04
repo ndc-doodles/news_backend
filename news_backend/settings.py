bfabd18f585d3e24efd4483764732a3e67b57018
doodles <nmedevelopercommunity@gmail.com>
2026-02-04 15:18:51 +0530

    port

--- news_backend/settings.py
+++ news_backend/settings.py
@@ -103,10 +103,11 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': 5432,  # <-- FORCE IT
     }
 }
+
 
 CSRF_TRUSTED_ORIGINS = [
     "http://newslive.cybexel.com",
