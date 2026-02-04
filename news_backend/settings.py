88d9a4da87cf78b960522da0682bc49eb452c176
doodles <nmedevelopercommunity@gmail.com>
2026-02-04 15:02:45 +0530

    database

--- news_backend/settings.py
+++ news_backend/settings.py
@@ -96,6 +96,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
 CSRF_TRUSTED_ORIGINS = [
     "http://newslive.cybexel.com",
     "http://www.newslive.cybexel.com",
